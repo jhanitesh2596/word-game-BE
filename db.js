eb92cca5ce4bba51c8f89ca3355882f71d973443
Nitesh Jha <reachtoniteshjha@gmail.com>
2025-07-17 12:40:46 +0530

    new commit

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ import mysql from 'mysql2/promise';
 
 
 export const pool = mysql.createPool({
-  host: 'localhost',     
-  user: 'root',         
-  password: '',
-  database: 'wordlyWord',  
+  host: process.env.DB_HOST,     
+  user: process.env.DB_USER,         
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,  
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
